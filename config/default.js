39eff46025e16705cdf509c3b7ea0109063324b0
Rob Pickering <rob@pickering.org>
2020-05-18 19:31:48 +0100

    Add optional media playback before bridge

--- config/default.js
+++ config/default.js
@@ -10,6 +10,11 @@ module.exports = {
         port: '7777',
         format: 'slin16'
     },
+    asterisk: {
+        // If set then Playback this Asterisk sound file before bridging to the dialogflow media.
+        // Useful for debug and breaking media stand-offs.
+        // playback: 'silence/1'
+    },
     mqtt: {
         url: 'mqtt://test.mosquitto.org',
         topicPrefix: 'dialogflow-asterisk'
